c6f741f864a9d73d3d904120cacc6e92340d9a2e
JeventJN <jeventjn@gmail.com>
2023-03-31 14:14:37 +0700

    HobbyPage+Assets

--- resources/views/hobby.blade.php
+++ resources/views/hobby.blade.php
@@ -6,83 +6,159 @@
   @vite('resources/css/app.css')
 </head>
 <body>
-    
-    <div class="relative bg-gray-50 dark:bg-slate-900 w-screen h-screen pattern">
-        <nav
-        class="z-20 flex shrink-0 grow-0 justify-around gap-4 border-t border-gray-200 bg-white/50 p-2.5 shadow-lg backdrop-blur-lg dark:border-slate-600/60 dark:bg-slate-800/50 fixed top-2/4 -translate-y-2/4 left-6 min-h-[auto] min-w-[64px] flex-col rounded-lg border"
-        >
-        <a
-            href="profile"
-            class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800"
-        >
-            <!-- HeroIcon - User -->
-            <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6 shrink-0"
+    <div class="relative w-screen h-screen bg-gradient-to-tl from-yellow-500 to-orange-500 ">
+        <div class="absolute flex h-screen items-center ml-[160px]">
+            <div class="relative mx-auto w-96 overflow-hidden rounded-[16px] p-[1px]">
+                <div class="rounded-[15px] bg-white bg-opacity-50 p-6">
+                    <div class="space-y-2">
+                        <img src="Assets/StravaRunning.jpeg" class="rounded-[15px]" alt="" />
+                        <p class="text-center text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-black-800 to-red-400">Runner!!!</p>
+                        <p class="font-md text-black-500 text-center">Running is my life. My life is about running. I've been running for 5 years. Everyday is new opprotunity to challenge myself and become a better runner. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="absolute flex h-screen items-center ml-[20px]">
+            <div class="absolute h-[455px] w-[250px] flex flex-col space-y-[19.25px]">
+                <div class="flex shadow-md rounded-xl ml-[570px] bg-white bg-opacity-50 items-center w-[250px] h-[80px] justify-content">
+                    <img src="Assets/ELITE.PNG" alt="" class="ml-[20px] w-16 h-16 rounded-full dark:bg-gray-500 aspect-square ml-4">
+                        <div class="space-y-4 text-center divide-y divide-gray-700">
+                        <div class="my-2 space-y-1">
+                            <h2 class="text-xs sm:text-2xl">Founder of</h2>
+                            <p class="px-5 text-xl font-black sm:text-base dark:text-gray-400">ELITE RTB CLUB</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex shadow-md rounded-xl ml-[570px] bg-white bg-opacity-50 items-center w-[250px] h-[80px] justify-content">
+                    <img src="Assets/1k.PNG" alt="" class="ml-[20px] w-16 h-16 rounded-full dark:bg-gray-500 aspect-square ml-4 ">
+                    <div class="space-y-4 text-center divide-y divide-gray-700">
+                        <div class="my-2 space-y-1">
+                            <h2 class="text-xs sm:text-2xl">Best Time</h2>
+                            <p class="animate-pulse hover:animate-none px-[60px] text-19xl font-black sm:text-base dark:text-gray-400">03:30</p>
+                        </div>
+                    </div>
+                </div>
+                        
+                <div class="flex shadow-md rounded-xl ml-[570px] bg-white bg-opacity-50 items-center w-[250px] h-[80px] justify-content">
+                    <img src="Assets/5k.PNG" alt="" class="ml-[20px] w-16 h-16 rounded-full dark:bg-gray-500 aspect-square ml-4 ">
+                    <div class="space-y-4 text-center divide-y divide-gray-700">
+                        <div class="my-2 space-y-1">
+                        <h2 class="text-xs sm:text-2xl">Best Time</h2>
+                            <p class="animate-pulse hover:animate-none px-[60px] text-19xl font-black sm:text-base dark:text-gray-400">23:38</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex shadow-md rounded-xl ml-[570px] bg-white bg-opacity-50 items-center w-[250px] h-[80px] justify-content">
+                    <img src="Assets/10k.PNG" alt="" class="ml-[20px] w-16 h-16 rounded-full dark:bg-gray-500 aspect-square ml-4 ">
+                    <div class="space-y-4 text-center divide-y divide-gray-700">
+                        <div class="my-2 space-y-1">
+                            <h2 class="text-xs sm:text-2xl">Best Time</h2>
+                            <p class="animate-pulse hover:animate-none px-[60px] text-19xl font-black sm:text-base dark:text-gray-400">55:37</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex shadow-md rounded-xl ml-[570px] bg-white bg-opacity-50 items-center w-[250px] h-[80px] justify-content">
+                    <img src="Assets/10k.PNG" alt="" class="ml-[20px] w-16 h-16 rounded-full dark:bg-gray-500 aspect-square ml-4 ">
+                    <div class="space-y-4 text-center divide-y divide-gray-700">
+                        <div class="my-2 space-y-1">
+                            <h2 class="text-xs sm:text-2xl">Best Time</h2>
+                            <p class="animate-pulse hover:animate-none px-[60px] text-19xl font-black sm:text-base dark:text-gray-400">55:37</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="absolute flex h-screen items-center ml-[885px]">
+            <img src="Assets/Statistic.png" alt="" class="h-[455px] rounded-[25px]">
+        </div>
+        
+        <div class="absolute flex h-screen items-center ml-[1130px] animate-pulse hover:animate-none">
+            <a href="https://www.strava.com/athletes/107733050">
+                <img src="Assets/Strava.png" alt="" class="rounded-[500px] h-[100px] hover:h-[110px]">
+                <div class="text-center font-black font=7xl">VISIT ME!!!</div>
+            </a>
+        </div>
+
+        <div class="relative bg-gray-50 dark:bg-slate-900 w-screen pattern">
+            <nav
+            class="z-20 flex shrink-0 grow-0 justify-around gap-4 border-t border-gray-200 bg-white/50 p-2.5 shadow-lg backdrop-blur-lg dark:border-slate-600/60 dark:bg-slate-800/50 fixed top-2/4 -translate-y-2/4 left-6 min-h-[auto] min-w-[64px] flex-col rounded-lg border"
             >
-            <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-            />
-            </svg>
-
-            <small class="text-center text-xs font-medium"> Profile </small>
-        </a>
-
-        <a
-            href="education"
-            class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800"
-        >
-            <!-- HeroIcon - Chart Bar -->
-            <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="-3 -5 24 24"
-            stroke-width="1"
-            stroke="currentColor"
-            class="w-9 h-9 shrink-0"
+            <a
+                href="profile"
+                class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-slate-800"
             >
-            <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5Z"
-            />
-            <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Z"/>
-            </svg>
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6 shrink-0"
+                >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                />
+                </svg>
 
-            <small class="text-center text-xs font-medium"> Education </small>
-        </a>
+                <small class="text-center text-xs font-medium"> Profile </small>
+            </a>
 
-        <a
-            href="hobby"
-            class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 bg-indigo-50 text-indigo-600 dark:bg-sky-900 dark:text-sky-50"
-        >
-        <!-- HeroIcon - Cog-6-tooth -->
-            <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="-5 -5 24 24"
-            stroke-width="1"
-            stroke="currentColor"
-            class="w-10 h-10 shrink-0"
+            <a
+                href="education"
+                class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 text-gray-700 hover:bg-gray-100 dark:text-gray-400 white:hover:bg-slate-800"
             >
-            <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M6.731 0 2 9.125h2.788L6.73 5.497l1.93 3.628h2.766L6.731 0zm4.694 9.125-1.372 2.756L8.66 9.125H6.547L10.053 16l3.484-6.875h-2.112z">
-            />
-            </svg>
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="-3 -5 24 24"
+                stroke-width="1"
+                stroke="currentColor"
+                class="w-9 h-9 shrink-0"
+                >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917l-7.5-3.5Z"
+                />
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466 4.176 9.032Z"/>
+                </svg>
 
-            <small class="text-center text-xs font-medium"> Hobby </small>
-        </a>
+                <small class="text-center text-xs font-medium"> Education </small>
+            </a>
+
+            <a
+                href="hobby"
+                class="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-1.5 bg-indigo-50 text-indigo-600 hover:bg-slate-800"
+            >
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="-5 -5 24 24"
+                stroke-width="1"
+                stroke="currentColor"
+                class="w-10 h-10 shrink-0"
+                >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6.731 0 2 9.125h2.788L6.73 5.497l1.93 3.628h2.766L6.731 0zm4.694 9.125-1.372 2.756L8.66 9.125H6.547L10.053 16l3.484-6.875h-2.112z">
+                />
+                </svg>
+
+                <small class="text-center text-xs font-medium"> Hobby </small>
+            </a>
+        </div>
     </div>
+    
 </body>
 </html>
