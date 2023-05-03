59301a96eaf5203a955313eae5d13b207b0d9835
JeventJN <jeventjn@gmail.com>
2023-05-03 08:49:34 +0700

    Back Button

--- resources/views/activity.blade.php
+++ resources/views/activity.blade.php
@@ -7,7 +7,7 @@
 <div class="flex items-center">
     <div class="flex justify-center text-[5vw] w-screen bg-gradient-to-tl from-blue-400 to-red-500">Activity List</div>
     <div class="absolute flex items-center">
-        <a href="/activity">
+        <a href="/hobby">
             <svg xmlns="http://www.w3.org/2000/svg" width="5vw" height="5vw" viewBox="0 0 24 24"><path fill="currentColor" d="m12 20l-8-8l8-8l1.425 1.4l-5.6 5.6H20v2H7.825l5.6 5.6L12 20Z"/></svg>
         </a>
     </div>
